7c7ed81d3d04976d3932ddf3f16572358490fc90
mfsaenz <fernandasaenz2216@gmail.com>
2023-08-17 12:49:31 -0500

    InicioProyecto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola</h1>
+    <nav>
+        <ul>
+            <a href="">Inicio</a>
+            <a href="{{ route('productos.index') }}">Productos</a>
+            <a href="">Servicios</a>
+            <a href="">Contactos</a>
+        </ul>
+    </nav>
+</body>
+</html>
